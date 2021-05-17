cbfd935b02dbff0a01cdf29f38ea788883b0cb2f
Andres <agomez.vg@gmail.com>
2021-05-16 22:41:56 -0400

    Se agregan funcionalidades de JavaScript, smooth scroll y varios. Todo funcionando Ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,16 @@
-$(".carousel").carousel();
+//Iniciar Carrusel con un tiempo de intervalo de 3 seg
+$(".carousel").carousel({ interval: 3000 });
+
+//Agregar clase a navbar cuando el scroll se desliza hacia abajo despues del carrusel
+$(window).scroll(function() {
+    var offset = $(window).scrollTop();
+
+    $(".navbar").toggleClass("trans", offset > 50);
+});
+// inicializar tooltip para la seccion quienes somos
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+//inicializar popover para la seccion destacados
+$('[data-toggle="popover"]').popover();
